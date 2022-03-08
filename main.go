dccbfe47537a1913259ceaee250634bf0316c82e
Eric Ekholm <eric.ekholm@gmail.com>
2022-03-08 06:55:04 -0500

    hello world gin test

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/test", func(c *gin.Context) {
+		c.JSON(200, gin.H{"ping": "pong"})
+	})
+
+	r.Run()
+}
